a48be982eb108c0897ec55962438a661dc3df781
ricduo <68867416+ricduo@users.noreply.github.com>
2020-07-27 19:01:11 +0100

    added tick need to fix why not working, how to split the sceen

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-radio.setGroup(99)
+radio.setGroup(22)
 basic.forever(function () {
-    basic.showIcon(IconNames.Yes)
     radio.sendValue("x", input.acceleration(Dimension.X))
     radio.sendValue("y", input.acceleration(Dimension.Y))
     radio.sendValue("z", input.acceleration(Dimension.Z))
+    basic.showIcon(IconNames.Yes)
 })
